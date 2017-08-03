8ddc7a271edeb70be3dbcb422c69102485cb4da6
Jean Huit <kollecter@gmail.com>
2017-08-03 14:15:52 +0000

    added check to make sure payment is filled before button is enabled

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
    $('select').material_select();
-})
+
+   $('.update_btn').attr('disabled',true);
+    $('.disabled_check input').keyup(function(){
+        if($(this).val().length !=0)
+            $('.update_btn').attr('disabled', false);
+        else
+            $('.update_btn').attr('disabled',true);
+    });
+});
