881a394110c2ce00b3a93a85634744be50c45a02
Jean Huit <kollecter@gmail.com>
2017-08-09 13:39:52 +0000

    cleaned out materializecss from js file

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-  // $(".button-collapse").sideNav();
-  //  $('select').material_select();
 
    $('.update_btn').attr('disabled',true);
     $('.disabled_check input').keyup(function(){
