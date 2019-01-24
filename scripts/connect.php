6a7e69912b7ec738bf045820479dbce3266056e0
Jean Huit <kollecter@gmail.com>
2019-01-24 10:51:20 +0000

    updates the connection string to use the env

--- scripts/connect.php
+++ scripts/connect.php
@@ -1,8 +1,12 @@
 <?php
 
-   define('DB_SERVER', $DB_SERVER);
-   define('DB_USERNAME', $DB_USERNAME);
-   define('DB_PASSWORD', $DB_PASSWORD);
-   define('DB_DATABASE', $DB_DATABASE);
-   $db = mysqli_connect($DB_SERVER,$DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
+  //  define('DB_SERVER', $DB_SERVER);
+  //  define('DB_USERNAME', $DB_USERNAME);
+  //  define('DB_PASSWORD', $DB_PASSWORD);
+  //  define('DB_DATABASE', $DB_DATABASE);
+  $DB_DATABASE = getenv('DB_DATABASE');
+  $DB_PASSWORD = getenv('DB_PASSWORD');
+  $DB_SERVER = getenv('DB_SERVER');
+  $DB_USERNAME = getenv('DB_USERNAME');
+  $db = mysqli_connect($DB_SERVER,$DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
  ?>
