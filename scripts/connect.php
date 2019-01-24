3a664bbc030517d0962f62ba7352252c9e2a3ec8
Jean Huit <kollecter@gmail.com>
2019-01-24 08:14:00 +0000

    new modification

--- scripts/connect.php
+++ scripts/connect.php
@@ -4,5 +4,5 @@
    define('DB_USERNAME', $DB_USERNAME);
    define('DB_PASSWORD', $DB_PASSWORD);
    define('DB_DATABASE', $DB_DATABASE);
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   $db = mysqli_connect($DB_SERVER,$DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
  ?>
