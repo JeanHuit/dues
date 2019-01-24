b202e2fe371f8e4d97c8f40f1f22d053e81eec4a
Jean Huit <kollecter@gmail.com>
2019-01-24 08:11:57 +0000

    modifies connection script to use config vars

---
+++ scripts/connect.php
@@ -0,0 +1,8 @@
+<?php
+
+   define('DB_SERVER', $DB_SERVER);
+   define('DB_USERNAME', $DB_USERNAME);
+   define('DB_PASSWORD', $DB_PASSWORD);
+   define('DB_DATABASE', $DB_DATABASE);
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+ ?>
