7e2849a88aac3a5fcb13c1ce8756660a74f1c2b3
Jean Huit <kollecter@gmail.com>
2019-01-31 11:21:50 +0000

    tries our cleardb addon from heroku

--- scripts/connect.php
+++ scripts/connect.php
@@ -1,9 +1,20 @@
 <?php
 
 
-  $DB_DATABASE = getenv('DB_DATABASE');
-  $DB_PASSWORD = getenv('DB_PASSWORD');
-  $DB_SERVER = getenv('DB_SERVER');
-  $DB_USERNAME = getenv('DB_USERNAME');
-  $db = mysqli_connect($DB_SERVER,$DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
+  // $DB_DATABASE = getenv('DB_DATABASE');
+  // $DB_PASSWORD = getenv('DB_PASSWORD');
+  // $DB_SERVER = getenv('DB_SERVER');
+  // $DB_USERNAME = getenv('DB_USERNAME');
+  // $db = mysqli_connect($DB_SERVER,$DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
+
+
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$dbase = substr($url["path"], 1);
+
+$db = new mysqli($server, $username, $password, $dbase);
  ?>
